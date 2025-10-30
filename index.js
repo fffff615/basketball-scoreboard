38d68133c5ae31531522bb73df7f69e5f18d5be6
yayyah <yahyasaleh16082013@gmail.com>
2025-10-30 14:01:13 +0000

    Added new game buttton to restart counters

--- index.js
+++ index.js
@@ -3,33 +3,40 @@ let homeGuestEl = document.getElementById('guest-score')
 let guestScore = 0
 let homeScore = 0
 
-function add1Home() {
+function add1Home () {
   homeScore += 1
   homeScoreEl.textContent = homeScore
 }
 
-function add2Home() {
+function add2Home () {
   homeScore += 2
   homeScoreEl.textContent = homeScore
 }
 
-function add3Home() {
+function add3Home () {
   homeScore += 3
   homeScoreEl.textContent = homeScore
 }
 
-function add1Guest() {
+function add1Guest () {
   guestScore += 1
   homeGuestEl.textContent = guestScore
 }
 
-function add2Guest() {
+function add2Guest () {
   guestScore += 2
   homeGuestEl.textContent = guestScore
 }
 
-function add3Guest() {
+function add3Guest () {
   guestScore += 3
   homeGuestEl.textContent = guestScore
 }
 
+function newGame () {
+  homeScore = 0
+  homeScoreEl.textContent = homeScore
+
+  guestScore = 0
+  homeGuestEl.textContent = guestScore
+}
